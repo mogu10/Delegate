ecd65e06495647a69d0c51b49d9171189ea4fed0
mogu10 <anton.cherepanov.1996@bk.ru>
2016-03-19 13:21:24 +0700

    простейшее использование делегата

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Print p = Print1;
+                p(i);
+            }
+            Console.ReadKey();
+        }
+
+        static void Print1(int i)
+        {
+            Console.WriteLine(i);
+        }
+
+        static void Print2(int j)
+        {
+            Console.WriteLine("* "+j+" *");
+        }
+
+        delegate void Print(int j);
+    }
+}
